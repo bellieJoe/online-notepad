c7ccf3cb25b9015dedd2fa4022ad4c77aa683342
Bellie Joe M. Jandusay <jandusayjoe14@gmail.com>
2022-07-08 16:29:21 +0800

    Applying respomsive design to the navigation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,17 +2,26 @@ import "./Nav.scss";
 import "../../Utils.scss";
 import logo from "../../logo.svg";
 
-import "./NavLogic";
+import { useState } from "react";
 
-function Nav(props){
+
+function Nav(){
+
+    const [menuToggle, setMenuToggle] = useState(false)
+
     return (
-
-        <div className="Nav">
+        <div className="Nav" id="Nav">
             <div className="container">
+                <div className="menu-con">
+                    <button onClick={() => setMenuToggle(true)}>
+                        <i className="fa-solid fa-bars"></i>
+                    </button>
+                </div>
                 <div className="logo-con">
                     <img src={logo} className="App-logo" alt="logo" />
                 </div>
-                <ul>
+                <ul className={menuToggle ? "menu-display" : "menu-hide"}>
+                    <button  onClick={() => setMenuToggle(false)}><i className="fa-solid fa-xmark"></i></button>
                     <li><i className="fa-solid fa-table-list space-after"></i> Notes</li>
                     <li><i className="fa-solid fa-user space-after"></i> Profile</li>
                     <li><i className="fa-solid fa-user-plus space-after"></i> Sign Up</li>
--- src/components/Nav/NavLogic.js
+++ src/components/Nav/NavLogic.js
@@ -1,1 +1,5 @@
-console.log("this is the nav logix")
+
+
+
+
+
